e6f773dad15176f1470c5904e525e26a40ed4bc3
Alex <alexmdq80@gmail.com>
2024-11-30 10:39:24 -0300

    Backend con await-try-catch /  promesas funcionando

--- index.js
+++ index.js
@@ -19,15 +19,93 @@ app.post("/user", async (req, res)=>{
 });
 
 app.get("/user", async (req, res)=>{
-    const users = await User.findAll();
+    const { filtro, nombre } = req.query;
+
+    let users;
+    console.log(filtro);
+    if (filtro === 'xNombre') {
+        console.log('Filtrado.');
+        users = await User.findAll( { where: { nombre } });
+    } else if (!filtro) {
+        console.log('Sin filtro.');
+        users = await User.findAll();
+    }
     res.status(200).json(users);
 });
 
-app.get("/user/:userId", async (req, res)=>{});
+//  CON TRY - CATCH
+// app.get("/user/:userId", async (req, res)=>{
+//     const { userId } = req.params;
 
-app.put("/user/:userId", async (req, res)=>{});
+//     try {
+//         const user = await User.findByPk(userId);
+//         if (!user) {
+//           throw new Error("Usuario no encontrado.");
+//         }
+//         res.status(200).json(user);
+//     } catch (error) {
+//         res.status(404).send(error.message);
+//     }
+   
+// });
+//  CON PROMESAS ******
+app.get("/user/:userId", (req, res)=>{
+    const { userId } = req.params;
 
-app.delete("/user/:userId", async (req, res)=>{});
+        User.findByPk(userId)
+        .then(user => {
+            if (!user) {
+              throw new Error("Usuario no encontrado.");
+            }
+            res.status(200).json(user);
+        })
+        .catch(error => {
+            res.status(404).send(error.message);
+        });
+});
+// ****************
+
+app.put("/user/:userId", async (req, res)=>{
+    const { userId } = req.params;
+    const { nombre, apellido, email } = req.body;
+
+    try {
+        const user = await User.findByPk(userId);
+        if (!user) {
+          throw new Error("Usuario no encontrado.");
+        };
+        const updated = await User.update( {nombre, apellido, email}, {
+            where: {id: userId},
+        } );
+
+        res.status(201).json(updated);
+
+    } catch (error) {
+        
+        res.status(error.message === "Usuario no encontrado." ? 404:500).send(error.message);
+   
+    }
+});
+
+app.delete("/user/:userId", async (req, res)=>{
+    const { userId } = req.params;
+
+    try {
+        const user = await User.findByPk(userId);
+        if (!user) {
+          throw new Error("Usuario no encontrado.");
+        };
+        await User.destroy( { where: { id: userId } } );
+        res.status(201).send(`El usuario ${user.nombre} fue eliminado`);
+
+    } catch (error) {
+        
+        res
+            .status(error.message === "Usuario no encontrado." ? 404:500)
+            .send(error.message);
+   
+    }
+});
 
 app.listen(PORT, async () => {
     await initDb();
